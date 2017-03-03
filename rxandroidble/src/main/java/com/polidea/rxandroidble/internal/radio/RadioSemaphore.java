9224de57192d8bdd1c6822992c6facf01b075c9f
Dariusz Seweryn <dariusz.seweryn@polidea.com>
2017-03-03 13:49:02 +0100

    Alternative implementation of RadioSemaphore. ~4x decrease in awaitRelease() execution time.

--- rxandroidble/src/main/java/com/polidea/rxandroidble/internal/radio/RadioSemaphore.java
+++ rxandroidble/src/main/java/com/polidea/rxandroidble/internal/radio/RadioSemaphore.java
@@ -3,19 +3,28 @@ package com.polidea.rxandroidble.internal.radio;
 
 import com.polidea.rxandroidble.internal.RadioAwaitReleaseInterface;
 import com.polidea.rxandroidble.internal.RadioReleaseInterface;
-import java.util.concurrent.Semaphore;
+import com.polidea.rxandroidble.internal.RxBleLog;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 class RadioSemaphore implements RadioReleaseInterface, RadioAwaitReleaseInterface {
 
-    private final Semaphore semaphore = new Semaphore(0);
+    private final AtomicBoolean isReleased = new AtomicBoolean(false);
 
     @Override
-    public void awaitRelease() throws InterruptedException {
-        semaphore.acquire();
+    public synchronized void awaitRelease() throws InterruptedException {
+        while (!isReleased.get()) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                RxBleLog.v(e, "Interrupted awaitRelease()");
+            }
+        }
     }
 
     @Override
-    public void release() {
-        semaphore.release();
+    public synchronized void release() {
+        if (isReleased.compareAndSet(false, true)) {
+            notify();
+        }
     }
 }
